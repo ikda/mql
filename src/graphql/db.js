e8c6f8d39ab45af7f56b2280134157b07a3a48a2
ikda <akusiron@gmail.com>
2019-03-28 23:21:40 +0900

    #10 Mutation bug fix

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -39,7 +39,7 @@ let movies = [
 export const getMovies = ()=> movies;
 
 export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    const filteredMovies = movies.filter(movie => movie.id === id);
     return filteredMovies[0];
 }
 
