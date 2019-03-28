006d2f08cf9a73bb8238ea85a6e60753578779b6
ikda <akusiron@gmail.com>
2019-03-28 23:20:53 +0900

    #10 Mutation

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -1,50 +1,64 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "nicolas",
-        age: "18",
-        gender: "male"
+        id: 0,
+        name: "movie name 0",
+        score: 1
     },
     {
-        id: "1",
-        name: "nicky",
-        age: "18",
-        gender: "female"
+        id: 1,
+        name: "movie name 1",
+        score: 8
     },
     {
-        id: "2",
-        name: "jack",
-        age: "18",
-        gender: "male"
+        id: 2,
+        name: "movie name 2",
+        score: 99
     },
     {
-        id: "3",
-        name: "tom",
-        age: "18",
-        gender: "female"
+        id: 3,
+        name: "movie name 3",
+        score: 25
+          },
+    {
+        id: 4,
+        name: "movie name 4",
+        score: 54
     },
     {
-        id: "4",
-        name: "zelda",
-        age: "18",
-        gender: "male"
+        id: 5,
+        name: "movie name 5",
+        score: 32
     },
     {
-        id: "5",
-        name: "jiro",
-        age: "18",
-        gender: "female"
-    },
-    {
-        id: "6",
-        name: "zidane",
-        age: "18",
-        gender: "male"
+        id: 6,
+        name: "movie name 6",
+        score: 78
     },
 ]
 
+export const getMovies = ()=> movies;
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    return filteredMovies[0];
+}
+
+export const deleteMovie = (id) => {
+    const cleanMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanMovies.length) {
+        movies = cleanMovies
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
