e62ccf28db41ebe082dc67d3dcbdf9e3c7708311
ikda <akusiron@gmail.com>
2019-04-01 17:36:51 +0900

    #12 wrapping rest api

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -1,64 +1,18 @@
-let movies = [
-    {
-        id: 0,
-        name: "movie name 0",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "movie name 1",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "movie name 2",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "movie name 3",
-        score: 25
-          },
-    {
-        id: 4,
-        name: "movie name 4",
-        score: 54
-    },
-    {
-        id: 5,
-        name: "movie name 5",
-        score: 32
-    },
-    {
-        id: 6,
-        name: "movie name 6",
-        score: 78
-    },
-]
+import fetch from "node-fetch";
 
-export const getMovies = ()=> movies;
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-export const deleteMovie = (id) => {
-    const cleanMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanMovies.length) {
-        movies = cleanMovies
-        return true;
-    } else {
-        return false;
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if (limit > 0) {
+        REQUEST_URL += `limit=${limit}`;
     }
+    if (rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 }
+    
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
